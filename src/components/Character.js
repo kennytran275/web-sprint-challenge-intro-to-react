32618b7fd57b5a2a1c386613c9c922501809e831
kennytran275 <kennyt275@gmail.com>
2021-03-19 15:50:49 -0400

    added a styled-component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,27 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components";
+
+const StyledChar = styled.div`
+  color: red;
+  border: 1px dashed black;
+  margin: 2%;
+
+  h1 {
+    color: black;
+  }
+`;
 
 const Character = (props) => {
-    const { char } = props;
-  
-    return (
-      <div>
-        <h1>{char.name}</h1>
-      </div>
-    );
-  };
-  
-  export default Character;
+  const { char } = props;
+
+  return (
+    <StyledChar>
+      <h1>Name: {char.name}</h1>
+      <h2>Gender: {char.gender}</h2>
+      <h3>Height: {char.height}</h3>
+    </StyledChar>
+  );
+};
+
+export default Character;
